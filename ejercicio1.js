410e049531badb0871f051592f8745fd0a1fe365
kanaima <ferrarijuanp@gmail.com>
2015-02-12 19:39:48 -0300

    new object auto

---
+++ ejercicio1.js
@@ -0,0 +1,24 @@
+var auto1 = {
+  "año": 1997,
+  "dni": 33274258,
+  "marca": "Ford"
+};
+
+var auto2 = {
+  "año": 2003,
+  "dni": 33274218,
+  "marca": "Chevrolet"
+};
+
+var auto3 = {
+  "año": 1997,
+  "dni": 33274123,
+  "marca": "Ford"
+};
+
+
+var auto4 = {
+  "año": 2010,
+  "dni": 10274123,
+  "marca": "Honda"
+};
