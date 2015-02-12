962340b60df3ae24e0c062b2cbc7ac4a3d383474
kanaima <ferrarijuanp@gmail.com>
2015-02-12 19:45:18 -0300

    quitando comillas

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,24 +1,24 @@
 var auto1 = {
-  "año": 1997,
-  "dni": 33274258,
-  "marca": "Ford"
+  año: 1997,
+  dni: 33274258,
+  marca: "Ford"
 };
 
 var auto2 = {
-  "año": 2003,
-  "dni": 33274218,
-  "marca": "Chevrolet"
+  año: 2003,
+  dni: 33274218,
+  marca: "Chevrolet"
 };
 
 var auto3 = {
-  "año": 1997,
-  "dni": 33274123,
-  "marca": "Ford"
+  año: 1997,
+  dni: 33274123,
+  marca: "Ford"
 };
 
 
 var auto4 = {
-  "año": 2010,
-  "dni": 10274123,
-  "marca": "Honda"
+  año: 2010,
+  dni: 10274123,
+  marca: "Honda"
 };
